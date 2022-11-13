bba8cbd20c128343c5a516354f97f109f30f756c
Luis Florez <braian1k3@gmail.com>
2022-11-13 16:35:11 -0500

    Crear carpeta del módulo 1 y subir el proyecto de los Ejercicios de Variables.

---
+++ Módulo1/EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,19 @@
+
+package ClasesVariables;
+
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String nombre="Luis";
+        double x=2.25;
+        final double dolar=4896.56;
+        float valPi=3.1416159F;
+        double euler=2.718281828459045;
+        
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Valor de 2 decimales: "+x);
+        System.out.println("Valor del dolar: "+dolar);
+        System.out.println("Valor de PI con los primeros 7 decimales: "+valPi);
+        System.out.println("Valor de Euler con los primeros 15 decimales: "+euler);
+    }
+}
---
+++ Módulo1/EjerciciosVariables/src/ClasesVariables/Ejercicio2.java
@@ -0,0 +1,23 @@
+
+package ClasesVariables;
+
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        double a=8;
+        char b='A';
+        String c="Colombia";
+        char d='A';
+        float e=74548F;
+        int numero1;
+        String nombre_Persona;
+        int edad;
+        String altura="20.3";
+        double alturaReal=Double.parseDouble(altura);
+        int doubl=96;
+        int aa=98;
+        double x=aa;
+        String y="25";
+        String g=y;
+    }
+}
